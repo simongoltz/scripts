fa42fb15352a9ae620e828383dc55ee984e3b524
simongoltz <me@simongoltz.com>
2022-02-12 22:51:16 +0100

    Add monitor ZPA App Connector

---
+++ Monitor ZPA App Connector connection status/monitor-zpa.ps1
@@ -0,0 +1,35 @@
+<#
+.SYNOPSIS
+  This script calls the ZPA API and returns the connections status of your specified connector. Please adapt to your needs
+.NOTES
+  Version:        1.0
+  Author:         Simon Goltz
+  Creation Date:  02-12-2022
+  Purpose/Change: Initial script development
+#>
+ 
+Add-Type -AssemblyName System.Web
+
+# Add your Parameters
+$client_id = "YourSuperLongClientID"
+$client_secret = `yourPassword`
+$customerId = ""
+$connectorId = "" 
+
+#Authentication
+$authBody = @{
+    client_id = $client_id
+    client_secret = $client_secret
+}
+
+$encodedAuthBody = [System.Web.HttpUtility]::UrlEncode($authBody)
+
+$authUri = "https://config.private.zscaler.com/signin"
+$authRequest = Invoke-RestMethod -Uri $authUri -Method Post -Body $encodedAuthBody
+$token = $authRequest.access_token
+
+#Query API for Connector Status
+$getConnector = "https://config.private.zscaler.com/mgmtconfig/v1/admin/customers/" + $customerId + "/connector/" + $connectorId
+$connector = Invoke-RestMethod -Uri $getConnector -Headers $requestHeader
+
+
